8c9c66e1c82421224f2b594f5d0ea40d310b5389
Paige Williams <paw145@humboldt.edu>
2018-11-02 09:08:07 -0700

    Add conditional to find cost based off of pizza size and number of toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,50 @@
 //Business Logic for pizza
 function Pizza(toppings, size){
-  this.toppings = [toppings],
-  this.size = size
-  this.cost = ""
+  this.toppings = toppings,
+  this.size = size,
+  this.cost = []
 }
 
-// Pizza.prototype.createPizza() {
-//
-// }
-//
-//
 Pizza.prototype.calcCost = function(size) {
   if (this.size === 10){
-    this.cost.push("$12");
+    if (this.toppings.length === 3 ){
+      this.cost.push("$14");
+      }
+    if (this.toppings.length === 2 ){
+      this.cost.push("$13");
+      }
+    if (this.toppings.length === 1){
+      this.cost.push("$12");
+      }
   }
-  else if (this.size === 12){
-    this.cost.push("$14");
+  if (this.size === 12){
+    if (this.toppings.length === 3 ){
+      this.cost.push("$16");
+      }
+    if (this.toppings.length === 2 ){
+      this.cost.push("$15");
+      }
+    if (this.toppings.length === 1){
+      this.cost.push("$14");
+      }
+  }
+  if (this.size === 18){
+    if (this.toppings.length === 3 ){
+      this.cost.push("$18");
+      }
+    if (this.toppings.length === 2 ){
+      this.cost.push("$17");
+      }
+    if (this.toppings.length === 1){
+      this.cost.push("$16");
+      }
   }
 }
 
 //User Interface Logic
 
-var pizza = new Pizza(["artichoke"], 10);
-var pizza2 = new Pizza(["peppers", "pepperoni"], 12);
+var pizza = new Pizza(["artichoke", "peppers", "spinach"], 10);
+// var pizza2 = new Pizza(["peppers"], 12);
 
 pizza.calcCost();
+// pizza2.calcCost();
