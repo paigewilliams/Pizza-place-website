42f56deea12db1241782c15cce83d2e50b21b7d8
Paige Williams <paw145@humboldt.edu>
2018-11-02 13:09:12 -0700

    Change calculation to find cost of pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -18,48 +18,51 @@ function Pizza(size, toppings){
 }
 
 Pizza.prototype.calcCost = function(size) {
-  if (this.size === 10){
-    if (this.toppings[0].length > 3 ){
-      this.cost.push("$16");
-      }
-    if (this.toppings[0].length === 3 ){
-      this.cost.push("$14");
-      }
-    if (this.toppings[0].length === 2 ){
-      this.cost.push("$13");
-      }
-    if (this.toppings[0].length === 1){
-      this.cost.push("$12");
-      }
-  }
-  if (this.size === 14){
-    if (this.toppings[0].length > 3 ){
-      this.cost.push("$18");
-      }
-    if (this.toppings[0].length === 3 ){
-      this.cost.push("$16");
-      }
-    if (this.toppings[0].length === 2 ){
-      this.cost.push("$15");
-      }
-    if (this.toppings[0].length === 1){
-      this.cost.push("$14");
-      }
-  }
-  if (this.size === 18){
-    if (this.toppings[0].length > 3 ){
-      this.cost.push("$20");
-      }
-    if (this.toppings[0].length === 3 ){
-      this.cost.push("$18");
-      }
-    if (this.toppings[0].length === 2 ){
-      this.cost.push("$17");
-      }
-    if (this.toppings[0].length === 1){
-      this.cost.push("$16");
-      }
-  }
+  var costOfPizza = this.size * (1.25) + this.toppings[0].length
+  this.cost.push(costOfPizza);
+
+  // if (this.size === 10){
+  //   if (this.toppings[0].length > 3 ){
+  //     this.cost.push("$16");
+  //     }
+  //   if (this.toppings[0].length === 3 ){
+  //     this.cost.push("$14");
+  //     }
+  //   if (this.toppings[0].length === 2 ){
+  //     this.cost.push("$13");
+  //     }
+  //   if (this.toppings[0].length === 1){
+  //     this.cost.push("$12");
+  //     }
+  // }
+  // if (this.size === 14){
+  //   if (this.toppings[0].length > 3 ){
+  //     this.cost.push("$18");
+  //     }
+  //   if (this.toppings[0].length === 3 ){
+  //     this.cost.push("$16");
+  //     }
+  //   if (this.toppings[0].length === 2 ){
+  //     this.cost.push("$15");
+  //     }
+  //   if (this.toppings[0].length === 1){
+  //     this.cost.push("$14");
+  //     }
+  // }
+  // if (this.size === 18){
+  //   if (this.toppings[0].length > 3 ){
+  //     this.cost.push("$20");
+  //     }
+  //   if (this.toppings[0].length === 3 ){
+  //     this.cost.push("$18");
+  //     }
+  //   if (this.toppings[0].length === 2 ){
+  //     this.cost.push("$17");
+  //     }
+  //   if (this.toppings[0].length === 1){
+  //     this.cost.push("$16");
+  //     }
+  // }
 }
 
 //User Interface Logic
